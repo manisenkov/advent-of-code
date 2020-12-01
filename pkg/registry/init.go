ce42e78aae59f603d7c02996f56a5f7c3357d708
Maksim Anisenkov <manisenkov@gmail.com>
2020-12-01 08:04:46 +0100

    2020, day 1

--- pkg/registry/init.go
+++ pkg/registry/init.go
@@ -1,13 +1,17 @@
 package registry
 
 import (
-	"github.com/manisenkov/advent-of-code/pkg/2018/solutions/day01"
-	"github.com/manisenkov/advent-of-code/pkg/2018/solutions/day02"
-	"github.com/manisenkov/advent-of-code/pkg/2018/solutions/day03"
+	day2018_01 "github.com/manisenkov/advent-of-code/pkg/2018/solutions/day01"
+	day2018_02 "github.com/manisenkov/advent-of-code/pkg/2018/solutions/day02"
+	day2018_03 "github.com/manisenkov/advent-of-code/pkg/2018/solutions/day03"
+
+	day2020_01 "github.com/manisenkov/advent-of-code/pkg/2020/solutions/day01"
 )
 
 func init() {
-	Register(2018, 1, new(day01.Solution))
-	Register(2018, 2, new(day02.Solution))
-	Register(2018, 3, new(day03.Solution))
+	Register(2018, 1, new(day2018_01.Solution))
+	Register(2018, 2, new(day2018_02.Solution))
+	Register(2018, 3, new(day2018_03.Solution))
+
+	Register(2020, 1, new(day2020_01.Solution))
 }
