21c3bba27e0f23f5a1e9cf4eff122817d98b3a31
Maksim Anisenkov <manisenkov@gmail.com>
2020-12-02 08:44:46 +0100

    2020, day 2

---
+++ pkg/2020/solutions/day02/day02.go
@@ -0,0 +1,76 @@
+package day02
+
+import (
+	"regexp"
+
+	"github.com/manisenkov/advent-of-code/pkg/common"
+)
+
+var inputRegex = regexp.MustCompile(`^(\d+)-(\d+) ([a-z]{1}): ([a-z]+)$`)
+
+type policy struct {
+	pos1, pos2 int
+	letter     rune
+}
+
+// Solution contains solution for day 2
+type Solution struct {
+	policies  []policy
+	passwords []string
+}
+
+func validatePasswordOld(pol policy, password string) bool {
+	letterBuckets := map[rune]int{}
+	for _, c := range password {
+		letterBuckets[c]++
+	}
+	return letterBuckets[pol.letter] >= pol.pos1 && letterBuckets[pol.letter] <= pol.pos2
+}
+
+func validatePasswordNew(pol policy, password string) bool {
+	sPassword := []rune(password)
+	return (sPassword[pol.pos1-1] == pol.letter && sPassword[pol.pos2-1] != pol.letter) ||
+		(sPassword[pol.pos1-1] != pol.letter && sPassword[pol.pos2-1] == pol.letter)
+}
+
+// Init initializes solution with input data
+func (s *Solution) Init(input []string) error {
+	policies := make([]policy, len(input))
+	passwords := make([]string, len(input))
+	for i, inp := range input {
+		m := inputRegex.FindAllStringSubmatch(inp, -1)
+		pol := policy{
+			pos1:   common.MustAtoi(m[0][1]),
+			pos2:   common.MustAtoi(m[0][2]),
+			letter: ([]rune(m[0][3]))[0],
+		}
+		password := m[0][4]
+		policies[i] = pol
+		passwords[i] = password
+	}
+	s.policies = policies
+	s.passwords = passwords
+	return nil
+}
+
+// Part1 .
+func (s *Solution) Part1() common.Any {
+	result := 0
+	for i, p := range s.passwords {
+		if validatePasswordOld(s.policies[i], p) {
+			result++
+		}
+	}
+	return result
+}
+
+// Part2 .
+func (s *Solution) Part2() common.Any {
+	result := 0
+	for i, p := range s.passwords {
+		if validatePasswordNew(s.policies[i], p) {
+			result++
+		}
+	}
+	return result
+}
--- pkg/registry/init.go
+++ pkg/registry/init.go
@@ -6,6 +6,7 @@ import (
 	day2018_03 "github.com/manisenkov/advent-of-code/pkg/2018/solutions/day03"
 
 	day2020_01 "github.com/manisenkov/advent-of-code/pkg/2020/solutions/day01"
+	day2020_02 "github.com/manisenkov/advent-of-code/pkg/2020/solutions/day02"
 )
 
 func init() {
@@ -14,4 +15,5 @@ func init() {
 	Register(2018, 3, new(day2018_03.Solution))
 
 	Register(2020, 1, new(day2020_01.Solution))
+	Register(2020, 2, new(day2020_02.Solution))
 }
