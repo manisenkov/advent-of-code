88f3b7848a49613569ed472ec77b3168b70ead0c
Maksim Anisenkov <manisenkov@gmail.com>
2020-12-05 11:58:09 +0100

    2020, day 5

--- pkg/common/util.go
+++ pkg/common/util.go
@@ -10,3 +10,14 @@ func MustAtoi(s string) int {
 	}
 	return res
 }
+
+// MustParseInt interprets a string s in the given base (0, 2 to 36) and bit
+// size (0 to 64) and returns the corresponding value.
+// It panics in case of error
+func MustParseInt(s string, base int, bitSize int) int64 {
+	res, err := strconv.ParseInt(s, base, bitSize)
+	if err != nil {
+		panic(err)
+	}
+	return res
+}
