fe357444bfdfbed21f45c3014f4db3ae2c4415c0
Maksim Anisenkov <manisenkov@gmail.com>
2020-12-07 12:52:23 +0100

    2020, day 7

---
+++ cmd/2020/07/07_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+const testInput1 = `
+light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.
+`
+
+const testInput2 = `
+shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.
+`
+
+type Day07TestSuite struct {
+	suite.Suite
+	sol *Solution
+}
+
+func (st *Day07TestSuite) SetupTest() {
+	st.sol = new(Solution)
+}
+
+func (st *Day07TestSuite) Test1() {
+	input := strings.Split(strings.Trim(testInput1, " \n"), "\n")
+	st.sol.Init(input)
+	st.Equal(4, st.sol.Part1())
+	st.Equal(32, st.sol.Part2())
+}
+
+func (st *Day07TestSuite) Test2() {
+	input := strings.Split(strings.Trim(testInput2, " \n"), "\n")
+	st.sol.Init(input)
+	st.Equal(0, st.sol.Part1())
+	st.Equal(126, st.sol.Part2())
+}
+
+func TestDay07(t *testing.T) {
+	st := new(Day07TestSuite)
+	suite.Run(t, st)
+}
