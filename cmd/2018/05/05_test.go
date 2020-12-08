44e0fa8cd3279646169e841dde6236d5bfcf5260
Maksim Anisenkov <manisenkov@gmail.com>
2020-12-08 01:48:28 +0100

    2018, day 5

---
+++ cmd/2018/05/05_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+const testInput = `dabAcCaCBAcCcaDA`
+
+type Day05TestSuite struct {
+	suite.Suite
+	sol *Solution
+}
+
+func (st *Day05TestSuite) SetupTest() {
+	st.sol = new(Solution)
+}
+
+func (st *Day05TestSuite) Test1() {
+	input := strings.Split(strings.Trim(testInput, " \n"), "\n")
+	st.sol.Init(input)
+	st.Equal(10, st.sol.Part1())
+	st.Equal(4, st.sol.Part2())
+}
+
+func TestDay05(t *testing.T) {
+	st := new(Day05TestSuite)
+	suite.Run(t, st)
+}
