bdf9d93e97fd2834998fd5aa839bd6f6182b09aa
Maksim Anisenkov <manisenkov@gmail.com>
2020-12-11 01:00:47 +0100

    2020, day 10, let's use int64

--- cmd/2020/10/10.go
+++ cmd/2020/10/10.go
@@ -8,16 +8,16 @@ import (
 
 // Solution contains solution for day 10
 type Solution struct {
-	jolts []int
+	jolts []int64
 }
 
 // Init initializes solution with input data
 func (sol *Solution) Init(input []string) {
-	sol.jolts = make([]int, len(input)+2)
+	sol.jolts = make([]int64, len(input)+2)
 	sol.jolts[0] = 0
-	maxJolt := 0
+	maxJolt := int64(0)
 	for i, inp := range input {
-		jolt := common.MustAtoi(inp)
+		jolt := common.MustParseInt(inp, 10, 64)
 		if jolt > maxJolt {
 			maxJolt = jolt
 		}
@@ -31,9 +31,9 @@ func (sol *Solution) Init(input []string) {
 
 // Part1 .
 func (sol *Solution) Part1() common.Any {
-	curJolt := 0
-	diff1Count := 0
-	diff3Count := 0
+	curJolt := int64(0)
+	diff1Count := int64(0)
+	diff3Count := int64(0)
 	for _, jolt := range sol.jolts {
 		if jolt-curJolt == 1 {
 			diff1Count++
@@ -48,7 +48,7 @@ func (sol *Solution) Part1() common.Any {
 
 // Part2 dynamic programming
 func (sol *Solution) Part2() common.Any {
-	numArrangements := make([]int, len(sol.jolts))
+	numArrangements := make([]int64, len(sol.jolts))
 	numArrangements[0] = 1
 	for i := 0; i < len(sol.jolts); i++ {
 		for j := i + 1; j < len(sol.jolts) && sol.jolts[j]-sol.jolts[i] <= 3; j++ {
@@ -62,7 +62,7 @@ func (sol *Solution) Part2() common.Any {
 // func (sol *Solution) Part2() common.Any {
 // 	i := 0
 // 	j := i + 1
-// 	res := 1
+// 	res := int64(1)
 // 	for j < len(sol.jolts) {
 // 		if sol.jolts[j]-sol.jolts[j-1] == 3 {
 // 			res *= sol.countArrangements(i, j)
@@ -75,11 +75,11 @@ func (sol *Solution) Part2() common.Any {
 // 	return res
 // }
 
-// func (sol *Solution) countArrangements(startIndex, endIndex int) int {
+// func (sol *Solution) countArrangements(startIndex, endIndex int) int64 {
 // 	if startIndex == endIndex {
 // 		return 1
 // 	}
-// 	res := 0
+// 	res := int64(0)
 // 	for i := startIndex + 1; i <= endIndex && sol.jolts[i]-sol.jolts[startIndex] <= 3; i++ {
 // 		res += sol.countArrangements(i, endIndex)
 // 	}
