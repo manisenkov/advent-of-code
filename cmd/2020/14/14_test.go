934d02bd95f097bf84559a2e1005df59fd998876
Maksim Anisenkov <manisenkov@gmail.com>
2020-12-14 19:41:33 +0100

    2020, day 14, part 2

--- cmd/2020/14/14_test.go
+++ cmd/2020/14/14_test.go
@@ -7,11 +7,18 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
-const testInput = `
+const testInput1 = `
 mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
 mem[8] = 11
 mem[7] = 101
 mem[8] = 0
+`
+
+const testInput2 = `
+mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1
 `
 
 type Day14TestSuite struct {
@@ -24,10 +31,15 @@ func (st *Day14TestSuite) SetupTest() {
 }
 
 func (st *Day14TestSuite) Test1() {
-	input := strings.Split(strings.Trim(testInput, " \n"), "\n")
+	input := strings.Split(strings.Trim(testInput1, " \n"), "\n")
 	st.sol.Init(input)
 	st.Equal(int64(165), st.sol.Part1())
-	st.Equal(0, st.sol.Part2())
+}
+
+func (st *Day14TestSuite) Test2() {
+	input := strings.Split(strings.Trim(testInput2, " \n"), "\n")
+	st.sol.Init(input)
+	st.Equal(int64(208), st.sol.Part2())
 }
 
 func TestDay14(t *testing.T) {
