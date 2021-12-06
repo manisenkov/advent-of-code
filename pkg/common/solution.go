05ac489b2c7c51139172750adc4f725a3f63fc9a
Maksim Anisenkov <manisenkov@gmail.com>
2021-12-06 13:22:26 +0100

    2021, day 6, slightly optimized

--- pkg/common/solution.go
+++ pkg/common/solution.go
@@ -3,7 +3,6 @@ package common
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
 	"time"
 )
@@ -24,15 +23,17 @@ func Run(sol DaySolution) {
 
 	startTime := time.Now()
 	sol.Init(input)
+	initTime := time.Now()
 	res1 := sol.Part1()
 	p1Time := time.Now()
 	res2 := sol.Part2()
 	p2Time := time.Now()
 	fmt.Printf("Part 1: %v\n", res1)
 	fmt.Printf("Part 2: %v\n", res2)
-	fmt.Printf(" - Part 1 time: %vms\n", math.Round(float64(p1Time.Sub(startTime).Nanoseconds()) / 1000) / 1000)
-	fmt.Printf(" - Part 2 time: %vms\n", math.Round(float64(p2Time.Sub(p1Time).Nanoseconds()) / 1000) / 1000)
-	fmt.Printf(" - Total time: %vms\n", math.Round(float64(p2Time.Sub(startTime).Nanoseconds()) / 1000) / 1000)
+	fmt.Printf(" - Init time: %vμs\n", float64(initTime.Sub(startTime).Nanoseconds())/1000)
+	fmt.Printf(" - Part 1 time: %vμs\n", float64(p1Time.Sub(initTime).Nanoseconds())/1000)
+	fmt.Printf(" - Part 2 time: %vμs\n", float64(p2Time.Sub(p1Time).Nanoseconds())/1000)
+	fmt.Printf(" - Total time: %vμs\n", float64(p2Time.Sub(startTime).Nanoseconds())/1000)
 }
 
 func fail(errorMessage string) {
