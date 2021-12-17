925741390c4c6e881ef6aacf259043e9270b69d6
Maksim Anisenkov <manisenkov@gmail.com>
2021-12-17 11:40:11 +0100

    2021, day 17

---
+++ cmd/2021/17/17.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"math"
+	"strings"
+
+	"github.com/manisenkov/advent-of-code/pkg/common"
+)
+
+// Solution contains solution for day 17
+type Solution struct {
+	minX, maxX, minY, maxY int
+	velocities             map[[2]int]bool
+}
+
+// Init initializes solution with input data
+func (sol *Solution) Init(input []string) {
+	s := strings.Split(strings.Split(input[0], ": ")[1], ", ")
+	xs := strings.Split(string([]byte(s[0])[2:]), "..")
+	sol.minX = common.MustAtoi(xs[0])
+	sol.maxX = common.MustAtoi(xs[1])
+	ys := strings.Split(string([]byte(s[1])[2:]), "..")
+	sol.minY = common.MustAtoi(ys[0])
+	sol.maxY = common.MustAtoi(ys[1])
+}
+
+// Part1 .
+func (sol *Solution) Part1() common.Any {
+	minVX := int(-1.0 + math.Sqrt(float64(1+8*sol.minX))/2.0)
+	if minVX*(minVX+1)/2 < sol.minX {
+		minVX++
+	}
+	maxVX := sol.maxX
+
+	maxVY := -0x7FFFFFFF
+	sol.velocities = map[[2]int]bool{}
+	for vx := minVX; vx <= maxVX; vx++ {
+		for vy := sol.minY; vy < 1000; vy++ {
+			if sol.shot(vx, vy) {
+				if vy > maxVY {
+					maxVY = vy
+				}
+				sol.velocities[[2]int{vx, vy}] = true
+			}
+		}
+	}
+	return maxVY * (maxVY + 1) / 2
+}
+
+// Part2 .
+func (sol *Solution) Part2() common.Any {
+	return len(sol.velocities)
+}
+
+func (sol *Solution) shot(vx, vy int) bool {
+	x := 0
+	y := 0
+	for y > sol.minY && (x < sol.minX || y > sol.maxY) {
+		x += vx
+		y += vy
+		if vx > 0 {
+			vx--
+		} else if vx < 0 {
+			vx++
+		}
+		vy--
+	}
+	return x >= sol.minX && x <= sol.maxX && y >= sol.minY && y <= sol.maxY
+}
+
+func main() {
+	common.Run(new(Solution))
+}
