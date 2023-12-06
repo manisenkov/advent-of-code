96ff0d434c108481c89661fe62952a10397a44af
Maksim Anisenkov <manisenkov@gmail.com>
2023-12-06 15:28:23 +0100

    2023, day 5

---
+++ py/tests/year2023/test_day05.py
@@ -0,0 +1,44 @@
+from io import StringIO
+
+from adventofcode.year2023.day05 import Day05
+
+
+INPUT = """seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4"""
+
+
+def test_day05():
+    sol = Day05(StringIO(INPUT))
+    assert sol.part1() == 35
+    assert sol.part2() == 46
