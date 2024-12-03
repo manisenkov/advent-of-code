5ef32a3a7fa31af4a3f58dcece0f11b7c405dcc8
Maksim Anisenkov <manisenkov@gmail.com>
2024-12-03 06:33:57 +0100

    2024, day 3

--- go/pkg/collections/slices.go
+++ go/pkg/collections/slices.go
@@ -1,5 +1,17 @@
 package collections
 
+// Filter applies predicate to each element of the given slice and return slice with the elements
+// where predicates returned true
+func Filter[T any](input []T, predicate func(T) bool) []T {
+	result := make([]T, 0)
+	for _, x := range input {
+		if predicate(x) {
+			result = append(result, x)
+		}
+	}
+	return result
+}
+
 // Map applies the given function to every item in a slice and return slice of results
 func Map[T, R any](input []T, mapper func(T) R) []R {
 	result := make([]R, len(input))
@@ -9,7 +21,7 @@ func Map[T, R any](input []T, mapper func(T) R) []R {
 	return result
 }
 
-// Return true if all items in a slice satisfy the given predicate
+// All returns true if all items in a slice satisfy the given predicate
 func All[T any](input []T, predicate func(T) bool) bool {
 	for _, item := range input {
 		if !predicate(item) {
