828ec23dd48d34822ec0fbb99f72b8ad298b82ca
Maksim Anisenkov <manisenkov@gmail.com>
2024-12-21 23:44:27 +0100

    2024, day 21, part 1

---
+++ go/cmd/2024/21/21_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+const testInput = `
+029A
+980A
+179A
+456A
+379A`
+
+type Day21TestSuite struct {
+	suite.Suite
+	sol *Solution
+}
+
+func (st *Day21TestSuite) SetupTest() {
+	st.sol = new(Solution)
+}
+
+func (st *Day21TestSuite) Test1() {
+	input := strings.Split(strings.Trim(testInput, " \n"), "\n")
+	st.sol.Init(input)
+	st.Equal(126384, st.sol.Part1())
+	st.Equal(0, st.sol.Part2())
+}
+
+func TestDay21(t *testing.T) {
+	st := new(Day21TestSuite)
+	suite.Run(t, st)
+}
